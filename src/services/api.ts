56be04c1027b63b85d5d480b04bfce55d7e483d1
Maksym Shchepetkov <mama2009113@gmail.com>
2025-06-17 13:55:35 +0300

    fix services bugs

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
+import type { Photos } from "../components/App/App";
 
 const key = "8OOPE-y2t239SntdN150iAqUHy5HuwDQFrKAKj6BEho";
 
-const handleGetPhotos = async (query, page) => {
-  const selectedPhotos = await axios.get(
+const handleGetPhotos = async (
+  query: string,
+  page: number
+): Promise<Photos> => {
+  const selectedPhotos = await axios.get<Photos>(
     "https://api.unsplash.com/search/photos",
     {
       params: {
